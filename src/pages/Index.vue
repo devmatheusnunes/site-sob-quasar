887800c31a1b8edb9351985d3747a62ad5a2badc
devmatheusnunes <dev.matheusnunes@gmail.com>
2022-03-22 11:38:04 -0300

    update code + add glasses

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,29 +4,29 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Prosto+One&display=swap" rel="stylesheet">
 
-    <!--MENU SUPERIOR-->
-    <div class="col-100 bg-yellow" style="heigth: 6vh">
-      <div class="row sides" style="justify-content: space-between;">
-          <img class="q-ma-md" src="~/src/assets/header/logo_superotica_horizontal_02.svg" alt="logo" style="max-width: 14vw;" />
+    <!--HEADER-->
+    <div class="header col-100 bg-yellow">
+      <div class="header-contents centralize row sides">
+          <img class="q-ma-md" src="~/src/assets/header/logo_superotica_horizontal_02.svg" alt="logo" />
 
-          <div style="display: flex; align-items: center; justify-content: center;">
+          <div class="header-icons">
             <a href="https://www.instagram.com/superoticabrasil/">
-              <img class="q-mx-sm" src="~/src/assets/header/instagram-bold.png" alt="instagram" style="max-width: 2.2vw;" />
+              <img class="q-mx-sm" src="~/src/assets/header/instagram-bold.png" alt="instagram"  />
             </a>
             <a href="https://www.facebook.com/SuperOticaBrasil">
-              <img class="q-mx-sm" src="~/src/assets/header/facebook.png" alt="facebook" style="max-width: 2.2vw;" />
+              <img class="q-mx-sm" src="~/src/assets/header/facebook.png" alt="facebook"  />
             </a>
             <a href="">
-              <img class="q-mx-sm" src="~/src/assets/header/whatsapp.png" alt="whatsapp" style="max-width: 2.2vw;" />
+              <img class="q-mx-sm" src="~/src/assets/header/whatsapp.png" alt="whatsapp" />
             </a>
           </div>
       </div>
     </div>
 
-    <!--MENU INFERIOR-->
-    <div class="menu-items col-100 bg-white" style="heigth: 4vh; display: flex; align-items: center; justify-content: center;">
-      <div class="row sides" style="justify-content: space-between;">
-        <div class="menu-btn" style="display: flex; align-items: center; justify-content: center;">
+    <!--MENU-->
+    <div class="menu-items centralize col-100 bg-white">
+      <div class="menu-case row sides">
+        <div class="menu-btn centralize">
           <q-btn class="btn" flat label="Sobre Nós" />
           <q-btn class="btn" flat label="Lentes" />
           <q-btn class="btn" flat label="Óculos" />
@@ -43,26 +43,27 @@
         <q-carousel-slide :name="2" img-src="~/src/assets/carousel/close-eye.jpg" />
         <q-carousel-slide :name="3" img-src="~/src/assets/carousel/doctor-glasses.jpg" />
         <q-carousel-slide :name="4" img-src="~/src/assets/carousel/baby-glasses.jpg" />
+        <q-carousel-slide :name="5" img-src="~/src/assets/carousel/man-glasses.jpg" />
       </q-carousel>
     </div>
 
     <!--LENSES-->
     <q-card class="bg-yellow col-100">
-      <q-card-sections class="img-lens sides">
+      <q-card-sections class="img-lens centralize sides">
         <img src="~/src/assets/lenses/essilor-logo.svg" alt="essilor">
         <img src="~/src/assets/lenses/hoya-logo.svg" alt="hoya">
         <img src="~/src/assets/lenses/kodaklens-logo.svg" alt="kodak">
         <img src="~/src/assets/lenses/varilux-logo.svg" alt="varilux">
         <img src="~/src/assets/lenses/zeiss-logo.svg" alt="zeiss">
       </q-card-sections>
-      <q-card-sections class="text-lens sides">
+      <q-card-sections class="text-lens centralize sides">
         <p>Trabalhamos com as melhores marcas nacionais e importadas</p>
       </q-card-sections>
     </q-card>
 
-    <!--O foco é você-->
-    <div style="max-width: 100%;">
-      <q-card class="my-card" flat bordered>
+    <!--CARD-YOU-->
+    <div class="col-100">
+      <q-card class="card-you" flat bordered>
         <q-card-section class="sides" horizontal>
           <q-card-section>
             <q-card-section class="title">
@@ -82,9 +83,9 @@
       </q-card>
     </div>
 
-    <!--Nosso Espaço-->
-    <div style="max-width: 100%">
-      <q-card class="my-card" flat bordered>
+    <!--CARD-SPACE-->
+    <div class="col-100">
+      <q-card class="card-space" flat bordered>
         <q-card-section class="sides" horizontal>
           <q-img
             class="image col-5"
@@ -102,6 +103,60 @@
         </q-card-section>
       </q-card>
     </div>
+
+    <!--CARD-GLASSES-->
+    <div class="col-100">
+      <div class="card-glasses">
+        <div class="title sides centralize">
+          Encontre seu perfil
+        </div>
+        <div class="sides">
+          <div class="card-glass-sections centralize">
+            <q-img src="~/src/assets/carousel/man-glasses.jpg" alt="man">
+              <div class="title-glass-img centralize">Masculino</div>
+            </q-img>
+            <q-img src="~/src/assets/carousel/woman-glasses.jpg" alt="woman">
+              <div class="title-glass-img centralize">Feminino</div>
+            </q-img>
+            <q-img src="~/src/assets/carousel/baby-glasses.jpg" alt="kid">
+              <div class="title-glass-img centralize">Infantil</div>
+            </q-img>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--Mais Vendidos
+    <div>
+      <q-card class="my-card-glasses">
+        <q-card-sections class="title">
+          Mais Vendidos
+        </q-card-sections>
+        <q-card-sections class="my-card-image">
+          <q-card-actions>
+            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Masculina
+              </div>
+            </q-img>
+          </q-card-actions>
+          <q-card-actions>
+            <q-img src="~/src/assets/carousel/woman-glasses.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Feminina
+              </div>
+            </q-img>
+          </q-card-actions>
+          <q-card-actions>
+            <q-img src="~/src/assets/carousel/baby-glasses.jpg">
+              <div class="absolute-bottom text-subtitle2 text-center">
+                Infantil
+              </div>
+            </q-img>
+          </q-card-actions>
+        </q-card-sections>
+      </q-card>
+    </div>-->
   </q-page>
 </template>
 
@@ -121,6 +176,7 @@ export default defineComponent({
 </script>
 
 <style scoped>
+/*GERAL*/
 .main {
   font-family: 'Prosto One', cursive;
 }
@@ -128,26 +184,58 @@ export default defineComponent({
   max-width: 980px;
   margin: 0 auto;
 }
-.menu-items .row .menu-btn .btn {
+.centralize {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.title {
+  color: #180081;
+  font-size: 5vh;
+  justify-content: space-around;
+}
+
+/*HEADER*/
+.header {
+  max-height: 12vh;
+}
+.header .header-contents {
+  justify-content: space-between;
+}
+.header .header-contents img {
+  max-width: 14vw;
+}
+.header .header-icons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.header .header-icons a img {
+  max-width: 2.2vw;
+}
+
+/*MENU*/
+.menu-items {
+  max-height: 6vh;
+}
+.menu-items .menu-case {
+  justify-content: space-between;
+}
+.menu-items .menu-case .menu-btn .btn {
   color: #b1b1b1;
 }
 .menu-items .row .menu-btn .btn:hover {
   color: #180081;
 }
 
+/*CAROUSEL*/
 .carousel {
   min-height: 82.5vh;
   background-size: cover;
 }
 
-.my-card .title {
-  color: #180081;
-  font-size: 5vh;
-  justify-content: space-around;
-}
+/*LENSES*/
 .img-lens {
-  display: flex;
-  align-items: center;
   justify-content: space-around;
   height: 15vh;
 }
@@ -156,11 +244,26 @@ export default defineComponent({
   max-width: 10vw;
 }
 .text-lens {
-  display: flex;
-  align-items: center;
   justify-content: space-around;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 3vh;
+  font-weight: bold;
+  text-transform: uppercase;
   color: #0c003f;
 }
+
+/*CARD-GLASSES*/
+.card-glasses .card-glass-sections q-img {
+  max-width: 30vw;
+}
+.title-glass-img {
+  width: 100%;
+  height: 100%;
+}
+
+.title-glass-img:hover {
+  background: none;
+  color: #0c003f;
+}
+
 </style>
